6e77bb73fe10dbcc058f314a28e0e001b285a7d1
Arulkumar <aruljagan8124@gmail.com>
2026-06-07 01:26:37 +0530

    index page updated

---
+++ javascript/Day1/assets/js/task.js
@@ -0,0 +1,181 @@
+var powerTurbine;
+console.log(powerTurbine)
+var weldingStaion;
+console.log(weldingStaion)
+var coolingTower;
+console.log(coolingTower)
+var boilerSystem;
+console.log(boilerSystem)
+var controlPanel;
+console.log(controlPanel)
+var sensorModule;
+console.log(sensorModule)
+var rollerCoaster;
+console.log(rollerCoaster)
+var schoolBus;
+console.log(schoolBus)
+var loveLetter;
+console.log(loveLetter)
+var modernTechnology;
+console.log(modernTechnology)
+var noteBooks;
+console.log(noteBooks);
+var strangerThings;
+console.log(strangerThings);
+var miniLaptop;
+console.log(miniLaptop)
+var shirtButton;
+console.log(shirtButton)
+var earBuds;
+console.log(earBuds)
+var zomotoBag;
+console.log(zomotoBag)
+var switchOn;
+console.log(switchOn)
+var windowShopping;
+console.log(windowShopping)
+var volvoBus;
+console.log(volvoBus)
+var treePlant;
+console.log(treePlant)
+
+
+
+let userName;
+console.log(userName)
+let spiderMan;
+console.log(spiderMan)
+let angryBird;
+console.log(angryBird)
+let tollFree;
+console.log(tollFree)
+let trafficPolice
+console.log(trafficPolice)
+let platformDeck
+console.log(platformDeck)
+let pipelineRoue;
+console.log(pipelineRoue)
+let shieldCover;
+console.log(shieldCover)
+let rackFrame;
+console.log(rackFrame)
+let iPad;
+console.log(iPad)
+let redmiMobile;
+console.log(redmiMobile)
+let rightHand;
+console.log(rightHand)
+let greenPark;
+console.log(greenPark)
+let worldWideWeb;
+console.log(worldWideWeb)
+let comedyDialogue;
+console.log(comedyDialogue)
+let stateBoard;
+console.log(stateBoard)
+let tunaFish;
+console.log(tunaFish)
+let chilliPowder;
+console.log(chilliPowder)
+let kunfuPanda;
+console.log(kunfuPanda)
+let superStar;
+console.log(superStar)
+let screenReader;
+console.log(screenReader)
+let washingMachine;
+console.log(washingMachine)
+let bladeCutter;
+console.log(bladeCutter)
+let chiefMinister;
+console.log(chiefMinister)
+let actionHero;
+console.log(actionHero)
+let tamilFolk;
+console.log(tamilFolk)
+let woodenBoard;
+console.log(woodenBoard)
+let operatingSystem;
+console.log(operatingSystem)
+let sunFlower;
+console.log(sunFlower)
+let sunRise;
+console.log(sunRise)
+let chickenBriyani;
+console.log(chickenBriyani)
+let dragonBooster;
+console.log(dragonBooster)
+let screenRecorder;
+console.log(screenRecorder)
+let photoShop;
+console.log(photoShop)
+let wicketKeeper;
+console.log(wicketKeeper)
+let redBus;
+console.log(redBus)
+let bluePrint;
+console.log(bluePrint)
+
+
+const powerStar;
+console.log(powerStar)
+const fireMan;
+console.log(fireMan)
+const ratHouse;
+console.log(ratHouse)
+const mangoFruit;
+console.log(mangoFruit)
+const cityGangster;
+console.log(cityGangster)
+const tableFan;
+console.log(tableFan)
+const trendBolt;
+console.log(trendBolt)
+const firstFloor;
+consol.log(firstFloor)
+const captainAmerica;
+console.log(captainAmerica)
+const basketBall;
+console.log(basketBall)
+const lemonRice;
+console.log(lemonRice)
+const roseGarden;
+console.log(roseGarden)
+const kingMaker;
+console.log(kingMaker)
+const tenThousands
+console.log(tenThousands)
+const filmDrector
+console.log(filmDrector)
+const lightGreen
+console.log(lightGreen)
+const fieldSet;
+console.log(fieldSet)
+const moneyTransfer;
+console.log(moneyTransfer)
+const governmentHospital;
+console.log(governmentHospital)
+const safetyJacket
+console.log(safetyJacket)
+const controlUnit
+console.log(controlUnit)
+const boatHouse
+console.log(boatHouse)
+const mensHostel
+console.log(mensHostel)
+const slowRace;
+console.log(slowRace)
+const luckyStar;
+console.log(luckyStar)
+const freedomFighter;
+console.log(freedomFighter)
+const radhaKrishna;
+console.log(radhaKrishna)
+const clockWise;
+console.log(clockWise)
+const softwareEngineer;
+console.log(softwareEngineer)
+const speedPost
+console.log(speedPost)
+const webOrder;
+console.log(webOrder)
